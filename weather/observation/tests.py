41ce4fcdb0c1d951d3d5ed92343b7dd47c3cad2d
Artem Balashov <balashov.artem@gmail.com>
2017-12-18 11:02:11 +0700

    Добавил тестов для проверки невалидных значений

--- weather/observation/tests.py
+++ weather/observation/tests.py
@@ -6,5 +6,13 @@ class ProviderTestCase(TestCase):
     def test_yandex_pogoda(self):
         WP.YandexPogoda.temperature_at_city('tomsk')
 
+    def test_yandex_pogoda_non_existent_city(self):
+        with self.assertRaises(Exception):
+            WP.YandexPogoda.temperature_at_city('123tomsk')
+
     def test_open_weather_map(self):
         WP.OpenWeatherMap.temperature_at_city('tomsk')
+
+    def test_open_weather_map_non_existent_city(self):
+        with self.assertRaises(Exception):
+            WP.OpenWeatherMap.temperature_at_city('123tomsk')
