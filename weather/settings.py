7ecfd7a670d956a4ee1753e8037f14eb4d1db224
Artem Balashov <balashov.artem@gmail.com>
2017-12-18 12:22:36 +0700

    Пробую поднять deploy на heroku без gunicorn

--- weather/settings.py
+++ weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't=5_f2t=h_oogp#j98-*@e32^*yb68!rvnz*1nn=tjs&24)9(t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
